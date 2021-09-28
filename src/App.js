4b48389ffa5538ef793fea7b7b091aa00a3db3f4
Olena40 <o.redchuk@gmx.de>
2021-09-28 16:18:23 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Data from './Data/mockData.json'
+
 
 function App() {
-  return (
+ 
+
+//for(hints elem : title){
+  //elem.println();
+//}
+
+
+
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <p>Hacker News</p>
+    <input />
+    <button >Search</button>
+    
+{Data.hits.map((article) =>  <div><a href = {article.url}>{article.title}</a></div>)}
+
     </div>
   );
 }
