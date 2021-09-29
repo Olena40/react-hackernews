ba04af6b4535af2f64fb58f2882723078a31c806
unknown <cborek@web.de>
2021-09-29 16:48:39 +0200

    The shit is ready!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,13 @@
 
 import './App.css';
-import Data from './Data/mockData.json'
+// import Data from './Data/mockData.json'
 import NewsArticle from './components/NewsArticle';
 
 function App() {
- 
-
-//for(hints elem : title){
-  //elem.println();
-//}
-
-
-
 
     return (
     <div className="App">
-    {/* <p>Hacker News</p>
-    <input />
-    <button >Search</button>
-    
-{Data.hits.map((article) =>  <div><a href = {article.url}>{article.title}</a></div>)} */}
-    <NewsArticle/>
+      <NewsArticle/>
     </div>
   );
 }
--- src/components/NewsArticle.js
+++ src/components/NewsArticle.js
@@ -1,39 +1,77 @@
 import React, {useState, useEffect} from 'react'
+import './NewsArticle.css';
 
 function NewsArticle() {
 
-    const [search, setSearch] = useState('linux');
+    const [search, setSearch] = useState();
     const [article, setArticle] = useState([]);
     const [input, setInput] = useState();
+    const [loading, setLoading] = useState(false);
+    const [page, setPage] = useState(1);
 
-
-console.log(article)
-    useEffect(() => {
-      fetch(`http://hn.algolia.com/api/v1/search?query=${search}`)
-        .then((res) => res.json())
-        .then((json) => setArticle(json.hits))
-        .catch((err) => console.log('Error!: ', err))
-    },[])
-
-    const handleSearch = (e) => {
-      e.preventDefault()
-      setSearch(input)
+    // Function to handle Loading spinner - L. 9, 22 
+    const loadingHandler = (state) => {
+      setLoading(state)
+      setTimeout(() => {setLoading(false); return loading}, 1500);
     }
 
+    // Fetching the data from API, with search and pagination
+    useEffect(() => {
+      fetch(`http://hn.algolia.com/api/v1/search?query=${search}&hitsPerPage=30&page=${page}`)
+        .then((res) => res.json())
+        .then(loadingHandler(true))
+        .then((json) => setArticle(json.hits))
+        .catch((err) => alert(err))
+    },[search, page])
 
+    // Handles the click on the search-button - L. 45
+    const handleSearch = (e) => {
+      e.preventDefault();
+      setSearch(input); // First put it into "input", then into search
+    }
+
+    // Slicing the dateString from API into day, month, year - L. 58
+    const dateFormatHandler = (dateString) => {
+      const day = dateString.substr(8, 2);
+      const month = dateString.substr(5, 2);
+      const year = dateString.substr(0, 4);
+
+      return `${day}.${month}.${year}`;
+    }
 
     return (
       <>
-        <div className="app">
-          <form onSubmit={handleSearch}>
+        {loading && <div className="loading-overlay"><div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div>}
+        <div className="header">
+          <form className="search" onSubmit={handleSearch}>
             <input type="search" id="searchInput" onChange={(e) => setInput(e.target.value)}/>
             <input type="submit" value="Search"/>
           </form>
         </div>
         <div className="container">
           <ul>
-            {article.map((i) =>  <li>{i.title}</li>)}
+            {article.map((i) => 
+              <li className="list-item">
+                {<a href="{i.link}">{i.title}</a>} <br/>
+                <ul className="sub-list">
+                  <li>Author: {i.author} | </li>
+                  <li>Created: {dateFormatHandler(i.created_at)} | </li>
+                  <li>topic: {search}</li>
+                </ul>
+              </li>
+            )}
           </ul>
+          <div className="pagination">
+            {(page <= 1) && (
+              <span className="page-down">◀️</span>
+            )}
+            {(page > 1) && (
+              <span className="page-down" onClick={() => setPage(page - 1)}>◀️</span>
+            )}
+            
+            <div className="pagination-count"> {page} </div>
+            <span className="page-up" onClick={() => setPage(page + 1)}>▶️</span>
+          </div>
         </div>
       </>
     )
